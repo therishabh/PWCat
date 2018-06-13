f48bc4961d49238b527e9bac6de82b8b20a737f7
Rishabh Agrawal <therishabhagrawal@gmail.com>
2018-06-13 17:16:46 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
+import { ImgCardComponent } from './img-card/img-card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/img-card/img-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img-card',
+  templateUrl: './img-card.component.html',
+  styleUrls: ['./img-card.component.scss']
+})
+export class ImgCardComponent implements OnInit {
+
+	message: string;
+	api: string;
+	fontsize: number;
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
